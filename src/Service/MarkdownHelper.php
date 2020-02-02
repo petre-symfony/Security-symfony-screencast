e62b1e0d98372b4fe80e42d332e0603abd4a6247
petrero <petrero@gmail.com>
2020-02-02 22:03:34 +0200

    1.1.Security & the User Class
    Yeaaaa! You've done it! You've made it to the tutorial where we get to build a security system with Symfony. This stuff is cool. Seriously, these days, the topic of security is gigantic! Just think about authentication: you might need to build a traditional login form, or a token-based API authentication system, or two-factor authentication or authentication across an API to a Single Sign-On server or something I've never even dreamed of before! For authorization, there are roles, access controls and more.
    Woh. So we're going to write some seriously fun code in this tutorial. And it will be especially fun, because there are some new cool toys in Symfony's security system that make it nicer than ever to work with.

---
+++ src/Service/MarkdownHelper.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Service;
+
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
+
+class MarkdownHelper{
+	private $cache;
+	private $markdown;
+	private $logger;
+	private $isDebug;
+
+	public function __construct(AdapterInterface $cache, MarkdownParserInterface $markdown, LoggerInterface $markdownLogger, bool $isDebug){
+		$this->cache = $cache;
+		$this->markdown = $markdown;
+		$this->logger = $markdownLogger;
+		$this->isDebug = $isDebug;
+	}
+
+	public function parse(string $source): string{
+		if (stripos($source, 'bacon') !== false) {
+			$this->logger->info('They are talking about bacon again!');
+		}
+
+		// skip caching entirely in debug
+		if ($this->isDebug) {
+			return $this->markdown->transformMarkdown($source);
+		}
+
+		$item = $this->cache->getItem('markdown_'.md5($source));
+		if (!$item->isHit()) {
+			$item->set($this->markdown->transformMarkdown($source));
+			$this->cache->save($item);
+		}
+
+		return $item->get();
+	}
+}
