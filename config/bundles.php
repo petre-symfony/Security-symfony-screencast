bdb48dd0dff9effc677bacb178be4c1d14f4db94
petrero <petrero@gmail.com>
2020-02-02 22:13:50 +0200

    1.2.Security & the User Class{composer require security}
    Version 1.7 of MakerBundle comes with a new command that will make our life much easier. Yep, there it is: 1.7. The new command is called make:user - try it:
    
    php bin/console make:user
    Ah! It explodes! Of course! Remember: in Symfony 4, our project starts small. If you need a feature, you need to install it. Run:
    
    composer require security
    Ah, check it out: this library has a recipe! When Composer finishes... find out what it did by running:
    
    git status
    A new config file! Check it out: config/packages/security.yaml. This file is super important. We'll start talking about it soon.

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Knp\Bundle\MarkdownBundle\KnpMarkdownBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
